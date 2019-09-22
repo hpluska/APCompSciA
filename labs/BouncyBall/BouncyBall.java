3b714ae68af5846ae66c09778d2e84866ebc54e0
hpluska <17304761+hpluska@users.noreply.github.com>
2019-09-22 15:43:24 -0600

    lab

--- labs/BouncyBall/BouncyBall.java
+++ labs/BouncyBall/BouncyBall.java
@@ -13,13 +13,14 @@ import javax.swing.Timer;
 /**
  * Animated program with a ball bouncing off the program boundaries
  * @author mvail
+ * @author hpluska
  * @author yourname
  */
 @SuppressWarnings("serial")
 public class BouncyBall extends JPanel
 {
-	private final int INIT_WIDTH = 600;
-	private final int INIT_HEIGHT = 400;
+	private final int INIT_WIDTH = 600; // width of the screen
+	private final int INIT_HEIGHT = 400;// height of screen
 	private final int DELAY = 60;       // milliseconds between Timer events
 
 	private final int DELTA_RANGE = 20; // range for xDelta and yDelta
@@ -51,38 +52,18 @@ public class BouncyBall extends JPanel
 
 		// Calculate new x anchor point value
 		x += xDelta;
-		// TODO: needs more to stay in-bounds
-
+		// TODO 1: needs more to stay in-bounds
+		if(x<0){xDelta*=-1;}else if(x > width){xDelta*=-1;};
+		
 
 		// Calculate new y anchor point value
 		y += yDelta;
-		// TODO: needs more to stay in-bounds
-		if(y > height){
-			yDelta*=-1;
-		}
-		if(x > width){
-			xDelta*=-1;
-		}
-		if( y < 0){
-			yDelta*=-1;
-		}
-		if(x < 0){
-			xDelta*=-1;
-		}
+		// TODO 2: needs more to stay in-bounds
+		if(y<0){yDelta*=-1;}else if(y > height){yDelta*=-1;}
+	
 
-
-		// TODO: use rDelta, MIN_RADIUS, and MAX_RADIUS to increase/decrease oval radius.
-		radius += rDelta;
-		if (radius > MAX_RADIUS) {
-			radius = MAX_RADIUS;
-			rDelta *= -1;
-		} else if (radius < MIN_RADIUS) {
-			radius = MIN_RADIUS;
-			rDelta *= -1;
-		}
-
-		
-		
+		// TODO 3: use rDelta, MIN_RADIUS, and MAX_RADIUS to increase/decrease oval radius.
+				
 
 
 		// Paint the ball at the calculated anchor point
@@ -105,29 +86,37 @@ public class BouncyBall extends JPanel
 		this.setDoubleBuffered(true);
 		setBackground(Color.black);
 
-		 // Instantiate instance variable for reuse in paintComponent()
-		rand = new Random();
+		// Initialize deltas (speed) for x and y
+		xDelta = 5;
+		yDelta = 5;
+		// TODO 4: replace xDelta and yDelta with random deltas from -DELTA_RANGE/2
+		// to +DELTA_RANGE/2
 
-		// Initialize ball color
-		color = Color.RED;
-		// TODO: replace with random starting color.
-
-		// Initialize ball anchor point within panel bounds
+		// Initialize ball starting point within panel bounds
 		x = INIT_WIDTH / 2;
 		y = INIT_HEIGHT / 2;
-		// TODO: replace centered starting point with a random
+
+		// TODO 5: replace centered starting point with a random
 		// position anywhere in-bounds - the ball should never
 		// extend out of bounds, so you'll need to take RADIUS
 		// into account
 
+		// Initialize ball color
+		color = Color.RED;
+		// TODO 6: replace with random starting color
+		//int R = generate a random number between 0 and 256
+                //int G = generate a random number between 0 and 256
+                //int B= generate a random number between 0 and 256
+                //color = new Color(R, G, B);
 
-		// Initialize deltas for x and y
-		xDelta = 5;
-		yDelta = 5;
-		// TODO: replace with random deltas from -DELTA_RANGE/2
-		// to +DELTA_RANGE/2
-		xDelta = (int)(Math.random()*(DELTA_RANGE/2-DELTA_RANGE/2)-DELTA_RANGE/2);
-		yDelta = (int)(Math.random()*(DELTA_RANGE/2-DELTA_RANGE/2)-DELTA_RANGE/2);
+		
+
+
+
+				
+			
+
+
 		//Start the animation - DO NOT REMOVE
 		startAnimation();
 	}
