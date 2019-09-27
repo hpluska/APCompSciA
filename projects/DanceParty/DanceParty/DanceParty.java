4db39faae53cc317cd3561c5c3b05baaedf53db3
hpluska <17304761+hpluska@users.noreply.github.com>
2019-09-27 05:52:58 -0600

    project

--- projects/DanceParty/DanceParty/DanceParty.java
+++ projects/DanceParty/DanceParty/DanceParty.java
@@ -31,119 +31,79 @@ public class DanceParty extends JPanel
 	 * Note: 100ms is 10 frames per second - you should not need
 	 * a faster refresh rate than this
 	 */
-        private final int DELAY = 200; //milliseconds
+        private final int DELAY = 100; //milliseconds
         /**
          * Constants to set the initial width and height of the screen
          */
         private final int INIT_WIDTH = 600; // width of the screen
 	private final int INIT_HEIGHT = 600;// height of screen
 	Graphics2D g;
-        
-        private int x, y, count=0, xDelta = 1, xEyes=1, xEyesDelta=1, offSet = 40, yDelta = 10, yMouth = 50;
-        int offSetAngle = 0;
-        int offSetAngle2 = 0;
+        /**
+	* Some variables to get you started.  
+	* 
+	*/
+        private int x, y, xDelta = 1, yDelta = 1, frameCount;
 
 	/**
-	 * Draws a filled bouncy ball that stays within the bounds of the screen.
+	 * Draws an avatar that dances to a song
 	 *
 	 * @param g Graphics context
 	 */
 	public void paintComponent(Graphics g0)
 	{
+	    //Creates a 2D graphics variable
+            //DO NOT MODIFY
+	    g = (Graphics2D) g0;
+
+
+            /**
+             * Below are just some examples of how you can draw stuff and get them moving
+	     *
+             */
 		
-            count++;
-            x += xDelta;
-            if(x > 300){
-                xDelta*=-1;
-            }
-            if(x < 100){
-                xDelta*=-1;
-            }
+	    //Counts the frames.  Useful for timing your movements.
+            //for example, you could say if(frameCount == 50){do something}
+	    frameCount++;
+
+            //Increments x by xDelta
+ 	    x += xDelta;
             
-            g = (Graphics2D) g0;
+	    //Creates a yellow rectangle that fills the screen 
             g.setColor(Color.YELLOW);
             g.fillRect(0,0, getWidth(), getHeight());
-            
+
+	    //Creates a gray rectangle that moves across the screen
+            //if the frameCount is greater than 50
+       	    if(frameCount > 50){
+            	g.setColor(Color.GRAY);
+            	g.fillRect(100 + x,100, 200, 200);
+            }
+	    
             //draws the name of the dancer
             g.setColor(Color.BLUE);
             g.setFont(new Font("Helvetica", Font.BOLD, 24));
-            g.drawString(name, getWidth()/2-100, 50);
+            g.drawString("My name is...", getWidth()/2-100, 50);
             
-            //draws the body
+            //draws a green oval in the middle of the screen
+            //notice the perspective from which the oval is drawn when you run the program
             g.setColor(Color.green);
-            g.fillOval(x, y, 200, 300);
+            g.fillOval(getWidth()/2, getHeight()/2, 200, 300);
             
-            //draws an eye with a pupil that moves back and forth
-            xEyes+=xEyesDelta;
-            if(xEyes==15){xEyesDelta *= -1;}
-            if(xEyes==0){xEyesDelta *= -1;}
-            g.setStroke(new BasicStroke(10));
+
+            //draws a polygon
+	    //x1 is an array of x coordinates
+	    //y1 is an array of y coordinates
             g.setColor(Color.black);
-            g.drawOval(x+90,178,35,10);
-            g.setColor(Color.white);
-            g.drawOval(x+95+xEyes, 180, 5, 5);
-            
-            //draws the horns
-            g.setColor(Color.black);
-            int x1[]={55+x, 55+x, 65+x};
+            int x1[]={55, 55, 65};
             int y1[]={105, 80, 90};
             g.drawPolygon( x1, y1, 3);
-            int x2[]={145+x, 135+x,145+x};
-            int y2[]={80, 90,105};
-            g.drawPolygon( x2, y2, 3);
-            
-            //draws the mouth
-            g.setColor(Color.black);
-            if(count > 100){
-            yMouth += yDelta;
-            }
-            g.fillOval(80+x, 310, 50, yMouth);
-                if(yMouth> 70)
-                    yDelta *= -1;
-                if(yMouth < 60)
-                    yDelta *= -1;
-            g.setColor(Color.red);
-            g.fillOval(83+x, 335, 44, 14); 
-            
-            //draws teeth
-            g.setColor(Color.WHITE);
-            g.fillRect(99+x, 310, 10, 12);
-            g.setStroke(new BasicStroke(1));
-            g.setColor(Color.black);
-            g.drawLine(104+x,310,104+x,322);
-            
-            //draws arms
+
+            //creates a stroke of width 10
             g.setStroke(new BasicStroke(10));
-            
-            if(count >50){
-            
-            g.drawLine(205+x, 250, x+250, 250);
-            g.drawLine(255+x, 250, x+255+(int)(Math.sin(Math.toRadians(offSetAngle+=20))*50), 250+(int)(Math.cos(Math.toRadians(offSetAngle+=20))*50));
-            g.drawLine(x-55, 250, x-5, 250);
-            g.drawLine(-55+x, 250, x+(-55)+(int)(Math.sin(Math.toRadians(offSetAngle2-=20))*50), 250+(int)(Math.cos(Math.toRadians(offSetAngle2-=20))*50));
-            }else{
-            g.drawLine(205+x,250,300+x,250);
-            g.drawLine(-95+x,250,-5+x,250);
-            }
-            
-            //draws legs
-            g.setColor(Color.BLACK);
-            g.setStroke(new BasicStroke(10));
-            g.drawLine(75+x, 400, 75+x, 450);
-            g.drawLine(125+x, 400, 125+x, 450);
-            if(count>50){
-                if(count%4==0){
-                    g.drawLine(75+x, 450, 100+x, 450);
-                    g.drawLine(125+x, 450, 150+x, 450);
-                }else{
-                    g.drawLine(75+x, 450, 50+x, 450);
-                    g.drawLine(125+x, 450, 100+x, 450);
-                }
-            }else{
- 
-                g.drawLine(75+x, 450, 50+x, 450);
-                g.drawLine(125+x, 450, 100+x, 450);
-            }
+
+            //draws a black line
+            g.setColor(Color.black);
+            g.drawLine(104,310,104,350);
             
             // Makes the animation smoother
             Toolkit.getDefaultToolkit().sync();
@@ -160,6 +120,7 @@ public class DanceParty extends JPanel
 		setPreferredSize(new Dimension(INIT_WIDTH, INIT_HEIGHT));
 		this.setDoubleBuffered(true);
 		setBackground(Color.black);
+		//TODO: Initialize x and y to what ever you want
                 x = 175;
                 y = 100;
 		startAnimation();
@@ -170,8 +131,11 @@ public class DanceParty extends JPanel
          */
         public static void playMusic(){
         //TODO: Change the path to the correct path on your computer
-        String fileName = "/home/fred/Desktop/TimberlineCS/RandomJavaProjects/NetbeansProjects/DanceParty/src/danceparty/MakeSomeNoise.au";
+        String fileName = "MakeSomeNoise.au";
         
+	/**
+	 * DO NOT MODIFY
+	 */
         try 
 	    {
                 
@@ -209,7 +173,7 @@ public class DanceParty extends JPanel
 	 */
 	public static void main (String[] args)
 	{
-		JFrame frame = new JFrame ("Bouncy Ball");
+		JFrame frame = new JFrame ("Dance Party");
 		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(new DanceParty());
 		frame.pack();
---
+++ projects/DanceParty/README.md
@@ -0,0 +1,44 @@
+# Connect Four
+
+## Project Overview
+In this project you will create a animation that moves within the boundaries of the screen.  
+
+## Objectives
+
+- [ ] Add code to a method in a given skeleton class
+- [ ] Use methods from the Graphics class.
+- [ ] Use the color class
+- [ ]Use variables to manage data and expressions to calculate size and position of your
+shapes.
+
+## Specifications
+
+Create an animated dance party. Your dance party should include at least one avatar (or object) which shouldmove in some way (back and forth, across the screen, up and down, wave a hand, etc). The dance party should include a variety of shapes and colors (these can move too). Your dance party should also
+include a message (Yo Yo, Woo Hoo, Rar, etc.).
+
+An example of a the dance party is provided here ([DanceParty.mp4](DanceParty.mp4))
+
+## Your Tasks
+
+- [ ] Begin a project in NetBeans called DanceParty
+- [ ] Locate the required class and add it to your DanceParty project folder
+	- [https://github.com/hpluska/APCompSciA/tree/master/projects/ConnectFour/ConnectFour](https://github.com/hpluska/APCompSciA/tree/master/projects/ConnectFour/ConnectFour)
+- [ ] Locate the ConnectFourGrid class
+- [ ] Complete this class, by completing the "TODO"'s indicated in this class.  Feel free to add more methods, or variables as needed to make a creative game!
+- [ ] Document your code thoroughly as you go!
+- [ ] Javadocs must be included with your final submission
+- [ ] Complete the required README using specified guidelines
+
+# Grade your project before submitting
+
+- [ ] Before you submit your project, grade your project against the criteria below.  CORRECTIONS WILL NOT BE ALLOWED. 
+	[ConnectFourRubric.md](ConnectFourRubric.md)
+- [ ] Submit your project using the following guidelines
+	- Create a folder in your Google Drive Account called <em>ConnectFourLastname</em> (Example: ConnectFourPluska)
+	- Share your folder with me: heidi.pluska@boiseschools.org
+	- Upload the following files
+		- ConnectFourGrid.java
+		- README
+
+
+
