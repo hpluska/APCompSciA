9634e66633480e9d12f3779a445049b504d66f18
hpluska <17304761+hpluska@users.noreply.github.com>
2019-11-02 17:54:13 -0600

    Set 13 notes

--- testing/test.java
+++ testing/test.java
@@ -2,10 +2,32 @@ public class test{
 
     public static void main(String args[]){
 
-         int unicode = 12345678;
-            String wString = "a";
-            char wChar = wString.charAt(0);//a now equals the character W
-            int wUnicode = wChar;
-            System.out.println(wUnicode);//prints 87
+       
+            for(int l = 35; l < 65; l++){
+
+                char c = (char)l;
+                System.out.println(c);
+            }
+
+            for(char l = '+'; l < '='; l++){
+
+                int i = l;
+                System.out.println(i);
+            }
+
+            String word = "tacocat1";
+            String reverse = "";
+            int lastChar = word.length()-1;
+            String msg = "";
+            while(lastChar >=0){
+                if(word.charAt(lastChar) != word.charAt(word.length()-1-lastChar)){
+                    msg = "NO!";
+                    break;
+                }else{
+                    msg = "Palindrome!";
+                }
+                lastChar--;
+            }
+            System.out.println(msg);
     }
 }
