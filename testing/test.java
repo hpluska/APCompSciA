c59b6abb6df2b98977c989fbd949f6a43ebe45ff
hpluska <17304761+hpluska@users.noreply.github.com>
2019-11-04 15:13:37 -0700

    adds

--- testing/test.java
+++ testing/test.java
@@ -15,11 +15,11 @@ public class test{
                 System.out.println(i);
             }
 
-            String word = "tacocat1";
+            String word = "tacocat";
             String reverse = "";
             int lastChar = word.length()-1;
             String msg = "";
-            while(lastChar >=0){
+            while(lastChar >=lastChar/2){
                 if(word.charAt(lastChar) != word.charAt(word.length()-1-lastChar)){
                     msg = "NO!";
                     break;
@@ -29,5 +29,29 @@ public class test{
                 lastChar--;
             }
             System.out.println(msg);
+
+int max = (int)Math.pow(2, 16);
+
+	    for(int l = 100; l < max;l++){
+		char c = (char)l;
+		System.out.print(c);
+
+		}
+
+String w = "Code is Cool!";
+String lc = "";
+char chart = 0;
+
+for(int c = 0; c < w.length(); c++){
+	if(w.charAt(c)>=65 && w.charAt(c) <= 95){
+		chart = (char)(w.charAt(c) + 32);
+	}else{
+		chart = w.charAt(c);
+	}
+
+	lc+=chart;
+
+}
+System.out.println(lc);
     }
 }
