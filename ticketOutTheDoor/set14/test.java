fa6fc680099562deb0a05d97f29f26fe085211a9
hpluska <17304761+hpluska@users.noreply.github.com>
2019-11-09 08:58:48 -0700

    adds

---
+++ ticketOutTheDoor/set14/test.java
@@ -0,0 +1,15 @@
+public class test{
+
+	public static void main(String ars[]){
+	
+	String s ="Take a Hike!";
+	String j = "idaho";
+
+	System.out.println(j.compareTo(s));
+
+	System.out.println((int)j.charAt(0));
+	System.out.println((int)s.charAt(0));
+
+	}
+
+}
