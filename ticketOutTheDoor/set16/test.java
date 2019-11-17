46ae577a39b85ee521b658c137f2b842f0de80af
hpluska <17304761+hpluska@users.noreply.github.com>
2019-11-17 11:30:45 -0700

    notes

--- ticketOutTheDoor/set16/test.java
+++ ticketOutTheDoor/set16/test.java
@@ -8,7 +8,13 @@ public class test{
 		System.out.println(houses[1]);//prints Kyle
 		System.out.println(houses[2]);//prints Bugs
 		System.out.println(houses[3]);//prints Marvin
-	
+
+            String s = "ENOUGH USE OF THE WORD THE" + "DUMMY";
+            String sp[] = s.split("THE");
+            System.out.println(sp.length-1);//prints 1
+	    System.out.println(sp[0]);
+            System.out.println(sp[1]);
+	    System.out.println(sp[2]);
 
 
 	}
