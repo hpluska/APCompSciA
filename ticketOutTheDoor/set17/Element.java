7344aab98638fdfce70ce76f6d2ca9fecaaf2d82
hpluska <17304761+hpluska@users.noreply.github.com>
2019-11-30 14:33:25 -0700

    notes

---
+++ ticketOutTheDoor/set17/Element.java
@@ -0,0 +1,13 @@
+public class Element{
+    String name;
+    double weight;
+    int protons, neutrons;
+
+    public Element(String n, double w, int p, int n){
+        name = n;
+        weight = w;
+        protons = p;
+        neutrons = n;
+
+    }
+}
---
+++ ticketOutTheDoor/set17/ElementMaker.java
@@ -0,0 +1,9 @@
+public class ElementMaker{
+
+    public static void main(String args[]){
+        Element element1 = new Element(args[0], 4, 2, 2);
+        Element element2 = new Element(args[1], 20, 10, 10);
+
+
+    } 
+}
