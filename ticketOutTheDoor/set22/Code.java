d523dcc8d1bcc6836c7746ee91952b85d0596905
hpluska <17304761+hpluska@users.noreply.github.com>
2020-01-21 06:43:03 -0700

    adds

---
+++ ticketOutTheDoor/set22/Code.java
@@ -0,0 +1,7 @@
+public class Code{
+
+    public static void main(String args[]){
+        String gradebook[][] = {{"5","4.5","3","4","3.5"}, {"2","3","2.5","4","R"}, {"4","1","3.5","5","4"}, {"5","2","3.5","3","4.5"}};
+    }
+
+}
