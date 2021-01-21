fa4f6c5f068b30d5df24512b45b35983ee1be0d5
hpluska <17304761+hpluska@users.noreply.github.com>
2021-01-21 06:22:25 -0700

    adds

---
+++ ticketOutTheDoor/set22/TVShowMaker.java
@@ -0,0 +1,7 @@
+public class TVShowMaker {
+    public static void main(String args[]){
+        TVShows walkingDead = new TVShows("walking dead");
+        TVShows southPark = new TVShows("south park");
+        System.out.println(TVShows.numShows);
+    }
+}
