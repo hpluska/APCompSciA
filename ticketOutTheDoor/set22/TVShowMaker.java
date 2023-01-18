a51a775843e1f28eaffd7eb75f2c6613418f8dd2
hpluska <17304761+hpluska@users.noreply.github.com>
2023-01-18 11:47:07 -0700

    edits

--- ticketOutTheDoor/set22/TVShowMaker.java
+++ ticketOutTheDoor/set22/TVShowMaker.java
@@ -1,7 +1,12 @@
 public class TVShowMaker {
     public static void main(String args[]){
-        TVShows walkingDead = new TVShows("walking dead");
-        TVShows southPark = new TVShows("south park");
+        TVShows show1 = new TVShows("Simpsons");
+        TVShows show2 = new TVShows("walking dead");
+        TVShows show3 = new TVShows("south park");
+        show3.showName = "Game of Thrones";
+
+        show2.numShows = 100;
+        System.out.println(show3.showName);
         System.out.println(TVShows.numShows);
     }
 }
