d995d198a247f6b15273e37e190c736ea64f2f92
hpluska <17304761+hpluska@users.noreply.github.com>
2023-09-29 15:51:04 -0600

    adds

--- ticketOutTheDoor/set8/test.java
+++ ticketOutTheDoor/set8/test.java
@@ -1,26 +1,29 @@
 public class test{
 
     public static void main(String args[]){
-        int x = 79, y = 46, z = -3; 
-        double d = 13.89, jj = 40.0; 
-        boolean b = true, c = false; 
-        
-        if(x == y && !(z < 0) || b && c){
-            System.out.println("this is if"); 
-        }else{
-            System.out.println("this is else"); 
-        } 
 
-        if( x != y && y==z && b || !c){
-            System.out.println("this is if"); 
-        }else{
-            System.out.println("this is else"); 
-        }
+        // int x = 79, y = 46, z = -3; 
+        // double d = 13.89, jj = 40.0; 
+        // boolean b = true, c = false; 
+        
+        // if(x == y && !(z < 0) || b && c){
+        //     System.out.println("this is if"); 
+        // }else{
+        //     System.out.println("this is else"); 
+        // } 
+
+        // if( x != y && y==z && b || !c){
+        //     System.out.println("this is if"); 
+        // }else{
+        //     System.out.println("this is else"); 
+        // }
 
         String a1 = "dog";
-        String b1 = "cat";
-        if(a1.equals(b1)){
+        String b1 = "dat";
+        if(a1 == b1){
             System.out.println("same");
+        }else{
+            System.out.println("not the same");
         }
     }
 }
