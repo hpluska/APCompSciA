a1960c06d8a2e206233c04b56ec5a846f5c17bdb
hpluska <17304761+hpluska@users.noreply.github.com>
2024-09-04 10:03:16 -0600

    adds

--- ticketOutTheDoor/set3/test.java
+++ ticketOutTheDoor/set3/test.java
@@ -1,7 +1,6 @@
 public class test{
     public static void main(String args[]){
-        System.out.println(79 + 3 * (4 + 82 - 68) -7 + 19);
-        System.out.println((179 + 21 + 10)/7 + 181);
-        int p = 40; int q = 4; System.out.println(2 + 8 * q/2 -p);
+        System.out.println(1 + 2 + "How are you " + (1 + 2)); 
+      
     }
 }
