5a1913457414c769b2d782f6992b61ef39b2576d
hpluska <17304761+hpluska@users.noreply.github.com>
2026-01-08 10:00:04 -0700

    updated website

--- examReviews/2425/ExamSets17thru18/CodedSolutions/Driver.java
+++ examReviews/2425/ExamSets17thru18/CodedSolutions/Driver.java
@@ -2,7 +2,9 @@ import java.text.ParseException;
 
 public class Driver {
     public static void main(String args[]){
-   
+        APCalendar c = new APCalendar();
+        int n =  c.dayOfWeek(2, 22, 1985);
+        System.out.println(n);
 
     }
     
